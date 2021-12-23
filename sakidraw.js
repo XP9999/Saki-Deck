9ae76fd21c4194672c64636a154ac78f0308c833
XP9999 <89538085+XP9999@users.noreply.github.com>
2021-12-23 14:42:05 +0630

    Update sakidraw.js

--- sakidraw.js
+++ sakidraw.js
@@ -1,8 +1,9 @@
-const deck = ['Aislinn Wishart', 'Amae Koromo', 'Atago Hiroe', 'Atarashi Ako', 'Choe Myeonghwa', 'Fukuji Mihoko', 'Hanada Kirame', 'Hao Huiyu', 'Haramura Nodoka', 'Hirose Sumire', 'Ikeda Kana', 'Inoue Jun', 'Iwato Kasumi', 'Jindai Komaki', 'Kajiki Yumi', 'Kanbara Satomi', 'Karijuku Tomoe', 'Kataoka Yuuki','Kosegawa Shiromi', 'Kunihiro Hajime', 'Matano Seiko', 'Matsumi Kuro', 'Matsumi Yuu', 'Maya Yukiko' , 'Megan Davin','Miyanaga Saki', 'Miyanaga Teru','Nanpo Kazue', 'Nelly Virsaladze','Onjouji Toki', 'Oohoshi Awai', 'Ryuumonbuchi Touka','Sagimori Arata', 'Sawamura Tomoki', 'Senoo Kaori', 'Shibuya Takami', 'Shimizudani Ryuuka', 'Someya Mako', 'Takakamo Shizuno', 'Takei Hisa', 'Takimi Haru', 'Toyouko Momoko', 'Usuzawa Sae', 'Usuzumi Hatsumi', 'Yumeno Maho']
-const eastDeck = ['Aislinn Wishart', 'Amae Koromo', 'Atago Hiroe', 'Atarashi Ako', 'Choe Myeonghwa', 'Fukuji Mihoko', 'Hanada Kirame', 'Hao Huiyu', 'Haramura Nodoka', 'Hirose Sumire', 'Ikeda Kana', 'Inoue Jun', 'Iwato Kasumi', 'Jindai Komaki', 'Kajiki Yumi', 'Kanbara Satomi', 'Karijuku Tomoe','Kosegawa Shiromi', 'Kunihiro Hajime', 'Matano Seiko', 'Matsumi Kuro', 'Matsumi Yuu', 'Maya Yukiko' , 'Megan Davin','Miyanaga Saki', 'Miyanaga Teru','Nelly Virsaladze','Onjouji Toki', 'Oohoshi Awai', 'Ryuumonbuchi Touka','Sagimori Arata', 'Sawamura Tomoki', 'Senoo Kaori', 'Shibuya Takami', 'Shimizudani Ryuuka', 'Someya Mako', 'Takakamo Shizuno', 'Takei Hisa', 'Takimi Haru', 'Toyouko Momoko', 'Usuzawa Sae', 'Usuzumi Hatsumi', 'Yumeno Maho']
+const deck = ['Aislinn Wishart', 'Amae Koromo', 'Anetai Toyone', 'Atago Hiroe', 'Atarashi Ako', 'Choe Myeonghwa', 'Fukuji Mihoko', 'Hanada Kirame', 'Hao Huiyu', 'Haramura Nodoka', 'Hirose Sumire', 'Ikeda Kana', 'Inoue Jun', 'Iwato Kasumi', 'Jindai Komaki', 'Kajiki Yumi', 'Kanbara Satomi', 'Karijuku Tomoe', 'Kataoka Yuuki','Kosegawa Shiromi', 'Kunihiro Hajime', 'Mase Yuuko', 'Matano Seiko', 'Matsumi Kuro', 'Matsumi Yuu', 'Maya Yukiko' , 'Megan Davin','Miyanaga Saki', 'Miyanaga Teru','Nanpo Kazue', 'Nelly Virsaladze','Onjouji Toki', 'Oohoshi Awai', 'Ryuumonbuchi Touka','Sagimori Arata', 'Sawamura Tomoki', 'Senoo Kaori', 'Shibuya Takami', 'Shimizudani Ryuuka', 'Someya Mako', 'Takakamo Shizuno', 'Takei Hisa', 'Takimi Haru', 'Toyouko Momoko', 'Ueshige Suzu', 'Usuzawa Sae', 'Usuzumi Hatsumi', 'Yumeno Maho']
+const eastDeck = ['Aislinn Wishart', 'Amae Koromo', 'Anetai Toyone'. 'Atago Hiroe', 'Atarashi Ako', 'Choe Myeonghwa', 'Fukuji Mihoko', 'Hanada Kirame', 'Hao Huiyu', 'Haramura Nodoka', 'Hirose Sumire', 'Ikeda Kana', 'Inoue Jun', 'Iwato Kasumi', 'Jindai Komaki', 'Kajiki Yumi', 'Kanbara Satomi', 'Karijuku Tomoe','Kosegawa Shiromi', 'Kunihiro Hajime', 'Mase Yuuko', 'Matano Seiko', 'Matsumi Kuro', 'Matsumi Yuu', 'Maya Yukiko' , 'Megan Davin','Miyanaga Saki', 'Miyanaga Teru','Nelly Virsaladze','Onjouji Toki', 'Oohoshi Awai', 'Ryuumonbuchi Touka','Sagimori Arata', 'Sawamura Tomoki', 'Senoo Kaori', 'Shibuya Takami', 'Shimizudani Ryuuka', 'Someya Mako', 'Takakamo Shizuno', 'Takei Hisa', 'Takimi Haru', 'Toyouko Momoko', 'Ueshige Suzu', 'Usuzawa Sae', 'Usuzumi Hatsumi', 'Yumeno Maho']
 const imglinks ={
     'Aislinn Wishart':"https://i.imgur.com/CvtF8qo.png",
     'Amae Koromo':"https://i.imgur.com/DQgpfCT.png",
+    'Anetai Toyone': "https://i.imgur.com/FMbxKg0.png",
     'Atago Hiroe':"https://i.imgur.com/s8kiJkt.png",
     'Atarashi Ako': "https://i.imgur.com/Bkvmsac.png",
     'Choe Myeonghwa':"https://i.imgur.com/q6JLuOO.png",
@@ -10,10 +11,10 @@ const imglinks ={
     'Hanada Kirame':"https://i.imgur.com/5C85O4n.png",
     'Hao Huiyu':"https://i.imgur.com/Of8NJ22.png",
     'Haramura Nodoka':"https://i.imgur.com/vby8ZNn.png",
-    'Hirose Sumire':"https://i.imgur.com/cn4AsHA.png",
+    'Hirose Sumire':"https://i.imgur.com/oHHxTJL.png",
     'Ikeda Kana':"https://i.imgur.com/Cf0a9BS.png",
-    'Inoue Jun':"https://i.imgur.com/nA5yjeb.png",
-    'Iwato Kasumi':"https://i.imgur.com/hbJMDB6.png",
+    'Inoue Jun':"https://i.imgur.com/h9gY0Pd.png",
+    'Iwato Kasumi':"https://i.imgur.com/SJ5vOfW.png",
     'Jindai Komaki':"https://i.imgur.com/oXspvUX.png",
     'Kajiki Yumi':"https://i.imgur.com/cZt6roH.png",
     'Kanbara Satomi':"https://i.imgur.com/gy6qkfa.png",
@@ -21,7 +22,8 @@ const imglinks ={
     'Kataoka Yuuki':"https://i.imgur.com/jpFNamF.png",
     'Kosegawa Shiromi':"https://i.imgur.com/tCxELMs.png",
     'Kunihiro Hajime':"https://i.imgur.com/43V7dxO.png",
-    'Matano Seiko':"https://i.imgur.com/AsSo0nL.png",
+    'Mase Yuuko':"https://i.imgur.com/v0qpUQa.png",
+    'Matano Seiko':"https://i.imgur.com/VFghmd0.png",
     'Matsumi Kuro':"https://i.imgur.com/c5bAPo6.png",
     'Matsumi Yuu':"https://i.imgur.com/7zefXpd.png",
     'Maya Yukiko':"https://i.imgur.com/0x1euJx.png",
@@ -43,6 +45,7 @@ const imglinks ={
     'Takei Hisa':"https://i.imgur.com/9vSxzh7.png",
     'Takimi Haru':"https://i.imgur.com/6F6L2tf.png",
     'Toyouko Momoko':"https://i.imgur.com/ZINeb9R.png",
+    'Ueshige Suzu':"https://i.imgur.com/3J7QfOa.png",
     'Usuzawa Sae':"https://i.imgur.com/kVvm3qB.png",
     'Usuzumi Hatsumi':"https://i.imgur.com/e1M5DSq.png",
     'Yumeno Maho':"https://i.imgur.com/NxbBN47.png"
